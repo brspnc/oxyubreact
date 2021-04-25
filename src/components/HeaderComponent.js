2087f849730c1171303fa3cd861916ebf77de7f4
Brisa <77309678+brspnc@users.noreply.github.com>
2021-04-24 18:38:56 -0700

    Login Modal

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, Jumbotron, NavbarToggler, Collapse, NavItem } from 'reactstrap'
+import { Nav, Navbar, Jumbotron, NavbarToggler, Collapse, NavItem,
+         Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap'
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
     constructor(props) {
         super(props);
 
-        this.toggleNav = this.toggleNav.bind(this);
         this.state={
-            isNavOpen:false
+            isNavOpen:false,
+            isModalOpen:false
         };
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
     }
         toggleNav() {
             this.setState({
                 isNavOpen: !this.state.isNavOpen
+            });
+        }
+
+        toggleModal() {
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
             });
         }
 
@@ -62,9 +73,38 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <span className="navbar-text ml-auto">
+                                <Button outline onClick={this.toggleModal}>
+                                    <i className="fa fa-sgin-in fa-lg" />Login
+                                </Button>
+                            </span>
                         </Collapse>
                     </div>
                 </Navbar>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handleLogin}>
+                            <div className="form-group">
+                                <Label htmlFor="username">Username: </Label>
+                                <Control.text id="username" name="username" 
+                                    model=".username"  />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="password">Password: </Label>
+                                <Control.text type="password" id="password" name="password" 
+                                    model=".password"/>
+                            </div>
+                            <div className="form-group">
+                                <Label check>
+                                 Remember me:  
+                                <Control.checkbox name="remember" model=".remember"/>
+                                </Label>
+                            </div>
+                            <Button type="submit" value="submit" color="dark">Login</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
